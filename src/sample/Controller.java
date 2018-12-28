3d123f7407dd5f186a57882552b47039c0cab3b1
Simone Claridge <simonec.023@gmail.com>
2018-12-28 01:21:57 -0800

    Initialize injectable fxml fields for the grid pane, combo box, display output label, get font button and close button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+
+    @FXML
+    private GridPane txtLabel;
+
+    @FXML
+    private ComboBox<?> fontComboBox;
+
+    @FXML
+    private Label displayLabel;
+
+    @FXML
+    private Button getFontButton;
+
+    @FXML
+    private Button closeButton;
+
 }

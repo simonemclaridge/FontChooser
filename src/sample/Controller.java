fac63cc9c717f2d5be76cf3e1f13faf0cedd53fe
Simone Claridge <simonec.023@gmail.com>
2018-12-28 01:30:06 -0800

    Change grid pane fx:id
    Add TextField txtField to Controller.java
    
    Name input text Label to txtLabel

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,12 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
     @FXML
-    private GridPane txtLabel;
+    private TextField txtField;
 
     @FXML
     private ComboBox<?> fontComboBox;
@@ -18,9 +18,11 @@ public class Controller {
     private Label displayLabel;
 
     @FXML
+    private Label txtLabel;
+
+    @FXML
     private Button getFontButton;
 
     @FXML
     private Button closeButton;
-
 }

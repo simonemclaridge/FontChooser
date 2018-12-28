5fa19fb2f86f9a89d6a82267ccf085d9462b9dd4
Simone Claridge <simonec.023@gmail.com>
2018-12-28 00:05:41 -0800

    Format Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,13 +12,13 @@ import java.io.FileInputStream;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Font Chooser");
         primaryStage.setScene(new Scene(root, 500, 500));
 
-       Image image = new Image(new FileInputStream("C:\\Users\\Simone\\IdeaProjects\\FontChooser\\src\\images\\earth-icon-1.png"));
-       primaryStage.getIcons().add(image);
+        Image image = new Image(new FileInputStream("C:\\Users\\Simone\\IdeaProjects\\FontChooser\\src\\images\\earth-icon-1.png"));
+        primaryStage.getIcons().add(image);
 
         primaryStage.show();
     }

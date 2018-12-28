a6f0488bb116165a422ca9664750e6ed506f1f31
Simone Claridge <simonec.023@gmail.com>
2018-12-28 00:04:17 -0800

    Add "requires transitive javafx.graphics" to module-info.java to debug
    Create new globe image
    
    Add globe icon image to GUI window

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,8 @@
 module FontChooser {
     requires transitive javafx.fxml;
     requires transitive  javafx.controls;
+    requires transitive javafx.graphics;
+
 
     opens sample;
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
 
 public class Main extends Application {
 
@@ -13,6 +16,10 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Font Chooser");
         primaryStage.setScene(new Scene(root, 500, 500));
+
+       Image image = new Image(new FileInputStream("C:\\Users\\Simone\\IdeaProjects\\FontChooser\\src\\images\\earth-icon-1.png"));
+       primaryStage.getIcons().add(image);
+
         primaryStage.show();
     }
 
